2e3ed0256da769f36e2312aee6473fcc0f5584e8
mstudent-nagoya <info@greycell.jp>
2021-10-25 16:07:58 +0900

    update time 2021/10/25 16:07 by ikeda

--- math.php
+++ math.php
@@ -31,11 +31,10 @@ $json_array = json_encode($imgs);
 <script type="text/javascript">
 let imgs = <?php echo $json_array; ?>;
 var classid = "<?php echo $classid ?>";
-console.log(imgs);
-console.log(classid);
+//console.log(imgs);
+//console.log(classid);
 </script>
 <script src="./js/jquery-3.6.0.min.js"></script>
-<script type="text/javascript" src="./js/notedrawing.js"></script>
 <script type="text/javascript" src="./js/mathtextbookpagechange.js"></script>
 
 <body>
@@ -55,17 +54,45 @@ console.log(classid);
             <button id="lastpage">最後へ</button>
         </div>
     </div>
-    <div id="noteside">note</button>
+    <div id="noteside">note
         <div id="note">
              <canvas id="canvas"></canvas>
-        </div><br>
-        <div id="center">
-        <input type="button" id="undo" name="undo" value="undo">
-        <input type="button" id="redo" name="redo" value="redo">
         </div>
-        
+        <div id="center">
+            <input type="button" id="undo" name="undo" value="undo">
+            <input type="button" id="redo" name="redo" value="redo">
+            <p>線の太さ<input type="range" min="1" max="15" value="3" id="lineWidth"><span id="lineNum">3</span></p>
+            <div id="cursor">
+            <ul>
+            <li style="background-color:#000000"></li>
+            <li style="background-color:#808080"></li>
+            <li style="background-color:#ffffff"></li>
+            <li style="background-color:#ffff00"></li>
+            <li style="background-color:#00ff00"></li>
+            <li style="background-color:#3eb370"></li>
+            <li style="background-color:#00552e"></li>
+            <li style="background-color:#0000ff"></li>
+            <li style="background-color:#4c6cb3"></li>
+            <li style="background-color:#00ffff"></li>
+            <li style="background-color:#ff00ff"></li>
+            <li style="background-color:#fce2c4"></li><br>
+            <li style="background-color:#ffa500"></li>
+            <li style="background-color:#f8b500"></li>
+            <li style="background-color:#ee7800"></li>
+            <li style="background-color:#ff0000"></li>
+            <li style="background-color:#eb6101"></li>
+            <li style="background-color:#eb6ea5"></li>
+            <li style="background-color:#884898"></li>
+            <li style="background-color:#7058a3"></li>
+            <li style="background-color:#965042"></li>
+            <li style="background-color:#6f4b3e"></li>
+            <li style="background-color:#c0c0c0"></li>
+            <li style="background-color:#e6b422"></li>
+            </ul>
+            </div>
+        </div>
     </div>
-
 </div>
+<script type="text/javascript" src="./js/notedrawing.js"></script>
 </body>
 </html>
